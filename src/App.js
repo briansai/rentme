944914dba9888e003d6379d9f8694b2e97ffc0ae
Brian Sai <brianwsai@gmail.com>
2023-02-12 17:05:06 -0800

    Implemented header section with logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Header />
+      </header>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from './Logo';
+import './Header.css';
+
+const Header = () => {
+  // create logo component
+  return (
+    <div className="header">
+      <Logo />
+    </div>
+  );
+
+  // create user profile component
+};
+
+export default Header;
