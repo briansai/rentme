cc94cea861e6bd327733b938d25500dd4d731081
Brian Sai <brianwsai@gmail.com>
2023-02-13 18:45:48 -0800

    Moved server file into its own folder

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
